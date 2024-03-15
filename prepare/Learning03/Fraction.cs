da822b588b6e7014b09ed6dfbd037375f4e6124f
PieroArbildo <89119204+PieroArbildo@users.noreply.github.com>
2024-03-15 00:52:47 -0500

    prepare Learning03 finished

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,49 @@
+public class Fraction
+{
+    private int _top;
+    private int _botton;
+
+    public Fraction()
+    {
+        _top =1;
+        _botton =1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _botton = 1;
+    }
+    public Fraction(int top,int botton)
+    {
+        _top = top;
+        _botton = botton;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+     public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBotton()
+    {
+        return _botton;
+    }
+     public void SetBotton(int botton)
+    {
+        _botton = botton;
+    }
+    
+    
+    public string GetFractionString()
+    {
+       return _top.ToString() +"/"+ _botton.ToString();
+    }
+    public double GetDecimalValue()
+    {
+       return Convert.ToDouble(_top)/Convert.ToDouble(_botton);
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+         Fraction numero1 = new Fraction();
+        Console.WriteLine(numero1.GetFractionString());
+        Console.WriteLine(numero1.GetDecimalValue());
+    
+        Fraction numero2 = new Fraction(5);   
+        Console.WriteLine(numero2.GetFractionString());
+        Console.WriteLine(numero2.GetDecimalValue());
+
+        Fraction numero3 = new Fraction(3,4);   
+        Console.WriteLine(numero3.GetFractionString());
+        Console.WriteLine(numero3.GetDecimalValue());
+
+        Fraction numero4 = new Fraction(1,3);   
+        Console.WriteLine(numero4.GetFractionString());
+        Console.WriteLine(numero4.GetDecimalValue());
     }
 }
